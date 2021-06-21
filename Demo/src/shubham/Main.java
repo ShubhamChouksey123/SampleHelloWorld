7593e6dd12140e1b8406a474a2df9067e4daf734
a-chandra <shubhamchouksey1998@gmail.com>
2021-06-21 12:39:41 +0530

    Fisrt Version

---
+++ Demo/src/shubham/Main.java
@@ -0,0 +1,13 @@
+package shubham;
+
+public class Main {
+	public static void main (String[] args) {
+		System.out.println("Hello Wolrd !");
+		for(int i = 1 ; i < 10 ; i++) {
+			for(int j = 0 ; j < i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
